78e6f7a124ad21a5d9107bbe70e770fd0b099831
Amit Kadam <amit.bk03@gmail.com>
2022-01-08 11:08:37 +0530

    🎨 Mobile respinsiveness added

--- src/components/Shifts/ShiftsWrapper.vue
+++ src/components/Shifts/ShiftsWrapper.vue
@@ -1,15 +1,16 @@
 <template>
   <div class="container shifts__wrapper">
-      <div class="row py-5">
-          <div class="col-md-12 d-flex">
+      <div class="row justify-content-center pt-5">
+          <div class="col-md-6 d-flex">
               <h3 class="fw-bold">Shifts</h3>
               <div class="ms-auto">
 
                 <button class="btn bg-dark text-white" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"> Add Shift</button>
               </div>
           </div>
-
-          <div class="shifts pt-4">
+      </div>
+      <div class="row justify-content-center">
+          <div class="col-md-6 shifts pt-4">
               <shift-view v-for="(shift,i) in shifts" :shift="shift" :key="i"/>
           </div>
       </div>
